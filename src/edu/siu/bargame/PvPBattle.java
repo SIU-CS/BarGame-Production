70f678cd79ae423974f1e5e0920b5fef33bebd67
Lucas Ambrose <lucasambrose@siu.edu>
2016-12-02 14:15:59 -0600

    add travis file

---
+++ src/edu/siu/bargame/PvPBattle.java
@@ -0,0 +1,28 @@
+package edu.siu.bargame;
+
+public class PvPBattle {
+	
+	
+	
+	protected void PlayerBattle(Character x, Character y){
+		
+	}
+	protected void PlayerLoot(Character x, Character y){
+		
+		
+		
+	}
+	protected int DamageCalc(int x, int y){
+		return 0;
+		//code to calculate damage based on each characters skills
+	}
+	protected void battleMenu(){
+		System.out.println("You have been challenged by another player\n");
+		
+	}
+	
+	
+	
+	
+
+}
